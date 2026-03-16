c8353e64e16301cc70e417f64333d13501208d00
kwphil <121273949+kwphil@users.noreply.github.com>
2026-03-16 23:23:51 +0000

    Laying out FFIs and implementing C ffi

---
+++ src/ffi/c_header.cpp
@@ -0,0 +1,205 @@
+/*
+   _____                 _                       
+  / ____|               | |                      
+ | |  __  ___  __ _ _ __| |     __ _ _ __   __ _ 
+ | | |_ |/ _ \/ _` | '__| |    / _` | '_ \ / _` | Clean, Clear and Fast Code
+ | |__| |  __/ (_| | |  | |___| (_| | | | | (_| | https://github.com/kwphil/gearlang
+  \_____|\___|\__,_|_|  |______\__,_|_| |_|\__, |
+                                            __/ |
+                                           |___/ 
+
+Licensed under the MIT License <https://opensource.org/licenses/MIT>.
+SPDX-License-Identifier: MIT
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <clang/AST/AST.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/ASTUnit.h>
+#include <clang/Tooling/Tooling.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Basic/Diagnostic.h>
+#include <llvm/Support/MemoryBuffer.h>
+
+#include <gearlang/ffi/c.hpp>
+#include <gearlang/ast/func.hpp>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <format>
+
+using std::vector;
+using std::unique_ptr;
+using std::string;
+using std::format;
+
+const vector<string> C_FFI_ARGS = {
+    "-I.",
+    "-std=c99"
+};
+
+Type c_builtin_to_gear(const clang::BuiltinType* ty) {
+    using clang::BuiltinType;
+    using Sem::Type;
+
+    switch(ty->getKind()) {
+        case BuiltinType::Int: return Type("i32");
+        case BuiltinType::Float: return Type("f32");
+        case BuiltinType::Double: return Type("f64");
+        case BuiltinType::Bool: return Type("bool");
+        case BuiltinType::Long: return Type("i64");
+        case BuiltinType::LongLong: return Type("128");
+        case Bui
+
+        default: throw std::runtime_error("ah"); 
+    }
+}
+
+Type c_to_gear_ty(clang::QualType* qt) {
+    using namespace clang;
+    using llvm::dyn_cast;
+
+    Sem::Type result;
+
+    auto ty = qt->getCanonicalType();
+
+    if(const auto* bt = dyn_cast<BuiltinType>(ty)) {
+        result = c_builtin_to_gear(bt);
+    } else if(const auto* pt = dyn_cast<PointerType>(ty)) {
+        QualType underlying = pt->getPointeeType();
+        result = c_to_gear_ty(&underlying).ref();
+    } else {
+        throw std::runtime_error("bleh");
+    }
+
+    return result;
+}
+
+class FunctionVisitor : public clang::RecursiveASTVisitor<FunctionVisitor> {
+public:
+    bool VisitFunctionDecl(clang::FunctionDecl* func) {
+        using clang::QualType;
+
+        if (func->isThisDeclarationADefinition() || func->isExternC()) {
+            using Ast::Nodes::Argument;
+
+            std::deque<unique_ptr<Argument>> args;
+            bool is_variadic = false;
+
+            for (unsigned i = 0; i < func->getNumParams(); ++i) {
+                QualType qt = func->getParamDecl(i)->getType();
+                
+                if (const auto* fn = qt->getAs<clang::FunctionProtoType>()) {
+                    is_variadic = fn->isVariadic();
+                    continue;
+                }
+
+                args.push_back(std::make_unique<Argument>(Argument(
+                    "",
+                    c_to_gear_ty(&qt),
+                    { 0, 0, 0, 0 }
+                )));
+            }
+
+            QualType ret = func->getReturnType();
+            Ast::Nodes::ExternFn fn(
+                func->getNameAsString(), 
+                c_to_gear_ty(&ret),
+                std::move(args),
+                is_variadic,
+                true,
+                { 0, 0, 0, 0 }   
+            );
+            
+            std::cout << fn.to_string();
+        }
+        return true;
+    }
+
+    bool VisitRecordDecl(clang::RecordDecl* record) {
+        if (record->isCompleteDefinition())
+            std::cout << "Struct: " << record->getNameAsString() << "\n";
+        return true;
+    }
+
+    bool VisitEnumDecl(clang::EnumDecl* enm) {
+        if (enm->isComplete())
+            std::cout << "Enum: " << enm->getNameAsString() << "\n";
+        return true;
+    }
+
+    bool VisitTypedefNameDecl(clang::TypedefNameDecl* td) {
+        std::cout << "Typedef: " << td->getNameAsString() << "\n";
+        return true;
+    }
+};
+
+class CFfiConsumer : public clang::ASTConsumer {
+public:
+    void HandleTranslationUnit(clang::ASTContext& context) override {
+        visitor.TraverseDecl(context.getTranslationUnitDecl());
+    }
+
+private:
+    FunctionVisitor visitor;
+};
+
+vector<unique_ptr<Ast::Nodes::NodeBase>> C_Ffi::compile_headers() {
+    vector<unique_ptr<clang::ASTUnit>> asts;
+
+    auto ast = clang::tooling::buildASTFromCodeWithArgs(
+        src.str(), 
+        C_FFI_ARGS,
+        "ffiwrap.c"
+    );
+
+    if (ast) {
+        CFfiConsumer consumer;
+        consumer.HandleTranslationUnit(ast->getASTContext());
+        asts.push_back(std::move(ast));
+    } else {
+        std::cerr << "Failed to parse C FFI: \n";
+    }
+
+    return { };
+}
+
+vector<std::string> splitStringstream(const string& input, char delimiter) {
+    vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(input);
+    while (std::getline(tokenStream, token, delimiter))
+        tokens.push_back(token);
+    return tokens;
+}
+
+void C_Ffi::add_header(string filename) {
+    auto split = splitStringstream(filename, '/');
+    if (split.size() == 1 || split[0] == "loc") {
+        add_header_to_file(format("#include \"{}\"", split.back()));
+    } else if (split[0] == "sys") {
+        add_header_to_file(format("#include <{}>", split[1]));
+    } else {
+        assert(false && "Unexpected header path!");
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+/*
+   _____                 _                       
+  / ____|               | |                      
+ | |  __  ___  __ _ _ __| |     __ _ _ __   __ _ 
+ | | |_ |/ _ \/ _` | '__| |    / _` | '_ \ / _` | Clean, Clear and Fast Code
+ | |__| |  __/ (_| | |  | |___| (_| | | | | (_| | https://github.com/kwphil/gearlang
+  \_____|\___|\__,_|_|  |______\__,_|_| |_|\__, |
+                                            __/ |
+                                           |___/ 
+
+Licensed under the MIT License <https://opensource.org/licenses/MIT>.
+SPDX-License-Identifier: MIT
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <cstdlib>
 #include <string>
 #include <iostream>
@@ -5,6 +37,7 @@
 #include <format>
 
 #include <gearlang/ast/base.hpp>
+#include <gearlang/ffi/c.hpp>
 #include <gearlang/lex.hpp>
 #include <gearlang/error.hpp>
 #include <gearlang/func.hpp>
@@ -100,6 +133,10 @@ static std::string compile_ir(const Options& opts) {
     });
 
     std::string output;
+
+    C_Ffi ffi_test;
+    ffi_test.add_header("sys/stdio.h");
+    ffi_test.compile_headers();
 
     RUN_STEP("rendering",
         output = ctx.render();
